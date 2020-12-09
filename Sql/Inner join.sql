0d608e67be846eaa4e6d2d3d73f7c7f02b6cec3e
Chandan Bhagat <chandan.bhagat@outlook.com>
2020-12-09 11:19:51 +0545

    sql file added

---
+++ Sql/Inner join.sql
@@ -0,0 +1,10 @@
+﻿select s.Id as StudentId, s.Name as StudentName, s.Email as StudentEmail, sd.Father as FatherName, sd.Mother as MotherName
+from Student s 
+inner join 
+(Select sf.StudentId, sf.GuardianName as Father, sm.GuardianName as Mother from StudentGuardian sf
+join StudentGuardian sm
+on sf.StudentId=sm.StudentId where sf.Type=0 and sm.Type=1) sd
+ on s.Id=sd.StudentId
+
+
+ Select * from StudentDetail
---
+++ Sql/StudentGUardian.sql
@@ -0,0 +1,30 @@
+﻿CREATE TABLE [dbo].[StudentGuardian]
+(
+	[Id] INT NOT NULL PRIMARY KEY Identity(1,1), 
+    [StudentId] INT NOT NULL, 
+    [GuardianName] NVARCHAR(50) NOT NULL, 
+    [Type] INT NOT NULL
+)
+
+declare @param1 as int 
+declare @param2 as int
+
+set @param1=1
+set @param2=2
+
+SELECT @param1, @param2
+
+	Select * from Student where id=@param1
+
+	Select * from Student where id=@param2
+
+
+--CRUD Operation
+	--Insert Query
+	Insert into Student (Name, Email)
+	values
+	('Student 1','test1@test.com'),
+	('Student 2','test2@test.com'),
+	('Student 3','test3@test.com')
+	
+	
